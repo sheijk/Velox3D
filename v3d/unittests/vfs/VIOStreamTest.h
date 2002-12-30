fa411947003e5f8ab81858296ed3084ec68caa0a
sheijk <sheijk@165746f0-bcf8-0310-b0ad-99815d7186ab>
2002-12-30 20:48:23 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@7 165746f0-bcf8-0310-b0ad-99815d7186ab

---
+++ v3d/unittests/vfs/VIOStreamTest.h
@@ -0,0 +1,14 @@
+#ifndef V3D_VIOSTREAMTEST_H
+#define V3D_VIOSTREAMTEST_H
+
+#include <UnitTests\VCommonTestBase.h>
+
+class VIOStreamTest : public VCommonTestBase  
+{
+public:
+	VIOStreamTest();
+	virtual ~VIOStreamTest();
+
+};
+
+#endif // V3D_VIOSTREAMTEST_H
