dc2ea8aef572ff859713b51c77188cc81f4d3187
sheijk <sheijk@165746f0-bcf8-0310-b0ad-99815d7186ab>
2003-03-16 18:36:23 +0000

    mehr tests
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@19 165746f0-bcf8-0310-b0ad-99815d7186ab

--- v3d/unittests/smartptr/VSmartPointerTest.cpp
+++ v3d/unittests/smartptr/VSmartPointerTest.cpp
@@ -11,6 +11,8 @@
 #include <Utils/SmartPtr/VPointerStorage.h>
 #include <Utils/SmartPtr/VExclusiveOwnerPol.h>
 
+#include <strstream>
+
 //////////////////////////////////////////////////////////////////////
 // Konstruktion/Destruktion
 //////////////////////////////////////////////////////////////////////
@@ -38,8 +40,8 @@ void VSmartPointerTest::ExecuteTest()
 	typedef VSmartPointer<
 		VDestructTest, 
 		VExclusiveOwnerPol< VPointerStorage<VDestructTest> >,
-		VNoCheckPol<VDestructTest>,
-		VNoCheckPol<VDestructTest>
+		VNoCheckPol<VDestructTest*>,
+		VNoCheckPol<VDestructTest*>
 		> OurPointer;
 
 	vbool bAlive;
@@ -55,11 +57,74 @@ void VSmartPointerTest::ExecuteTest()
 			VError);
 	}
 
-	// test reset functions
-
 	// test release functions
+	ptr.Release();
+
+	if( ptr.Get() != 0 ) 
+	{
+		V3D_THROW_UNITTEST_ERROR(
+			"VSmartPointer<..>.Release() did not free the subject",
+			VError);
+	}
+
+	// test reset functions
+	bAlive = true;
+	ptr.Reset(new VDestructTest(bAlive));
+	vbool bAlive2;
+	ptr.Reset(new VDestructTest(bAlive2));
+
+	if( true == bAlive )
+	{
+		V3D_THROW_UNITTEST_ERROR(
+			"VSmartPointer<..>.Reset(..) did not delete the old subject",
+			VError);
+	}
+
+	if( false == bAlive2 )
+	{
+		V3D_THROW_UNITTEST_ERROR(
+			"VSmartPointer<..>.Reset(..) deleted the new subject",
+			VError);
+	}
 
 	// test copy functions
+	vbool bAlive3;
+
+	VDestructTest* pDestrTest = new VDestructTest(bAlive3);
+
+	ptr.Release();
+	ptr.Reset(pDestrTest);
+	OurPointer ptr2(0);
+
+	ptr2 = ptr;
+
+	if( ptr2.Get() != pDestrTest )
+	{
+		V3D_THROW_UNITTEST_ERROR(
+			"VSmartPointer<..>.op=(..): destination does not own the pointer",
+			VError);
+	}
+
+	if( ptr.Get() != 0 )
+	{
+		V3D_THROW_UNITTEST_ERROR(
+			"VSmartPointer<..>.op=(..): source was not set to 0",
+			VError);
+	}
+
+	// check size of smart pointer
+	if( sizeof(ptr) != 4 ) 
+	{
+		std::strstream strm;
+		strm << "sizeof(VSmartPointer<..>) is " << sizeof(ptr)
+			<< " instead of 4 bytes";
+
+		strm << " , sizeof(SmartHandle): " << sizeof(ptr.m_Handle);
+
+		std::string str = strm.str();
+
+		V3D_THROW_UNITTEST_ERROR(str.c_str(), VWarning);
+	}
 
 	// try to access a member function
 }
