1b2dbf4b8cebacac00458d9663b9eea44c39c6ea
insane <insane@165746f0-bcf8-0310-b0ad-99815d7186ab>
2004-01-02 17:42:10 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@156 165746f0-bcf8-0310-b0ad-99815d7186ab

--- velox/Source/Console/VConsoleFrame.cpp
+++ velox/Source/Console/VConsoleFrame.cpp
@@ -80,7 +80,7 @@ void VTextControl::OnEnter(wxKeyEvent& event)
 
 VConsoleFrame::VConsoleFrame() : wxFrame ((wxFrame *) NULL, -1,
 										  "Velox Console",wxPoint(50, 50),
-										  wxSize(250, 556), wxCAPTION, "frame")
+										  wxSize(250, 700), wxCAPTION, "frame")
 {
 	Register();
 
