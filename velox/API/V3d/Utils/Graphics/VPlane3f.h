7d171f29e31c51c020995ee5e5e64114e75ea3f5
insane <insane@165746f0-bcf8-0310-b0ad-99815d7186ab>
2004-01-30 00:32:23 +0000

    added small plane class
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@168 165746f0-bcf8-0310-b0ad-99815d7186ab

---
+++ velox/API/V3d/Utils/Graphics/VPlane3f.h
@@ -0,0 +1,41 @@
+#ifndef V3D_VPLANE3F_H
+#define V3D_VPLANE3F_H
+//-----------------------------------------------------------------------------
+#include <v3d/Core/VCoreLib.h>
+#include <v3d/Utils/Graphics/VVertex3f.h>
+//-----------------------------------------------------------------------------
+namespace v3d {
+namespace graphics {
+//-----------------------------------------------------------------------------
+
+/**
+ * A plane consisting of a vertex and distance to coordinate system
+ * @author ins
+ */
+
+struct VPlane3f
+{
+	VVertex3f m_Normal;
+	vfloat32 m_fDistance;
+
+	VPlane3f()
+	{
+		m_fDistance = 0;
+	}
+
+	VPlane3f(vfloat32 in_X, vfloat32 in_Y, vfloat32 in_Z, vfloat32 in_Distance)
+	{
+		m_Normal.x = in_X;
+		m_Normal.y = in_Y;
+		m_Normal.z = in_Z;
+
+		m_fDistance = in_Distance;
+	}
+};
+
+//-----------------------------------------------------------------------------
+} // namespace graphics
+} // namespace v3d
+//-----------------------------------------------------------------------------
+#endif // V3D_VPLANE3F_H
+
