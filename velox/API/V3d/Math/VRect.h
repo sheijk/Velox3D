8df1fb98fce5dc6edb14ca325fce2bead8165433
sheijk <sheijk@165746f0-bcf8-0310-b0ad-99815d7186ab>
2004-07-04 15:10:03 +0000

    nachtrag / fix
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@300 165746f0-bcf8-0310-b0ad-99815d7186ab

---
+++ velox/API/V3d/Math/VRect.h
@@ -0,0 +1,53 @@
+#ifndef V3D_VRECT_2004_06_19_H
+#define V3D_VRECT_2004_06_19_H
+//-----------------------------------------------------------------------------
+#include <v3d/Core/VCoreLib.h>
+
+//-----------------------------------------------------------------------------
+namespace v3d { namespace math {
+//-----------------------------------------------------------------------------
+using namespace v3d; // prevent auto indenting
+
+/**
+ * A simple rectangle
+ *
+ * @author sheijk
+ */
+template<typename Scalar>
+class VRect
+{
+public:
+	typedef Scalar ScalarType;
+
+	VRect(ScalarType l, ScalarType t, ScalarType r, Scalar b)
+	{
+		left = l;
+		top = t;
+		right = r;
+		bottom = b;
+	}
+
+	ScalarType GetWidth() const		{ return right - left; }
+	ScalarType GetHeight() const	{ return bottom - top; }
+
+	Scalar GetLeft() const		{ return left; }
+	Scalar GetRight() const		{ return right; }
+	Scalar GetTop()	const		{ return top; }
+	Scalar GetBottom() const	{ return bottom; }
+
+	void SetLeft(Scalar l)		{ left = l; }
+	void SetRight(Scalar r)		{ right = r; }
+	void SetTop(Scalar t)		{ top = t; }
+	void SetBottom(Scalar b)	{ bottom = b; }
+
+private:
+	ScalarType left;
+	ScalarType top;
+	ScalarType right;
+	ScalarType bottom;
+};
+
+//-----------------------------------------------------------------------------
+}} // namespace v3d::math
+//-----------------------------------------------------------------------------
+#endif // V3D_VRECT_2004_06_19_H
