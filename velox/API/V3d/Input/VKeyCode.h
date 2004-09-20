ce514287e1f7fd469630ec9f88d96bc589fc2196
Lord_Adres <Lord_Adres@165746f0-bcf8-0310-b0ad-99815d7186ab>
2004-09-20 21:10:07 +0000

    T'schulding
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@336 165746f0-bcf8-0310-b0ad-99815d7186ab

---
+++ velox/API/V3d/Input/VKeyCode.h
@@ -0,0 +1,123 @@
+#ifndef V3D_KEYENUMERATION_2004_08_09_H
+#define V3D_KEYENUMERATION_2004_08_09_H
+//-----------------------------------------------------------------------------
+#include <v3d/Core/VCoreLib.h>
+
+//-----------------------------------------------------------------------------
+namespace v3d {
+namespace input {
+//-----------------------------------------------------------------------------
+
+enum KeyCodes {
+	KeyEscape,
+	KeyReturn,
+	KeyBackSpace,
+	KeyTab,
+	KeyLeftControl,
+	KeyRightControl,
+	KeyLeftAlt,
+	KeyRightAlt,
+	KeyLeftShift,
+	KeyRightShift,
+	KeyUp,
+	KeyLeft,
+	KeyRight,
+	KeyDown,
+	KeyHome,
+	KeyEnd,
+	KeyPageUp,
+	KeyPageDown,
+	KeyInsert,
+	KeyDelete,
+	KeyBackSlash,
+	KeyMinus,
+	KeyEquals,
+	KeyLeftBracket,
+	KeyRightBracket,
+	KeySemicolon,
+	KeyApostrophe,
+	KeyGrave,
+	KeyComma,
+	KeyPeriod,
+	KeySlash,
+	KeyMultiply,
+	KeyNumLock,
+	KeyScrollLock,
+	KeySYSRQ,
+	KeySpace,
+	KeyCapital,
+
+	Key1,
+	Key2,
+	Key3,
+  	Key4,
+	Key5,
+	Key6,
+	Key7,
+	Key8,
+	Key9,
+	Key0,
+  
+	KeyA,
+	KeyB,
+	KeyC,
+	KeyD,
+	KeyE,
+	KeyF,
+    	KeyG,
+    	KeyH,
+	KeyI,
+	KeyJ,
+    	KeyK,
+   	KeyL,
+	KeyM,
+	KeyN,
+	KeyO,
+	KeyP,
+	KeyQ,
+	KeyR,
+	KeyS,
+	KeyT,
+	KeyU,
+	KeyV,
+	KeyW,
+    	KeyX,
+	KeyY,
+	KeyZ,
+
+    	KeyF1,
+    	KeyF2,
+    	KeyF3,
+    	KeyF4,
+    	KeyF5,
+    	KeyF6,
+    	KeyF7,
+    	KeyF8,
+    	KeyF9,
+    	KeyF10,
+	KeyF11,
+	KeyF12
+//  Key_Subtract,  /* - on numeric keypad */
+//  Key_Add,  /* + on numeric keypad */
+//  Key_Numpad1,
+//  Key_Numpad2,
+//  Key_Numpad3,
+//	Key_Numpad4,
+//	Key_Numpad5,
+//	Key_Numpad6,
+//	Key_Numpad7,
+//	Key_Numpad8,
+//	Key_Numpad9,
+//  Key_Numpad0,
+//  Key_Decimal,    /* . on numeric keypad */
+//	Key_NumpadEnter, /* Enter on numeric keypad */
+//  Key_NumpadComma,   /* , on numeric keypad (NEC PC98) */
+//  Key_Divide,   /* / on numeric keypad */
+};
+
+
+//-----------------------------------------------------------------------------
+} // namespace input
+} // namespace v3d
+//-----------------------------------------------------------------------------
+#endif // V3D_KEYENUMERATUIN_2004_08_09_H
