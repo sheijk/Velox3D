a7d2e2d6c761509947de0c63962ab5f0bbabb4f3
Lord_Adres <Lord_Adres@165746f0-bcf8-0310-b0ad-99815d7186ab>
2004-09-22 15:55:16 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@340 165746f0-bcf8-0310-b0ad-99815d7186ab

--- velox/API/V3dLib/Utils/VStringValue.h
+++ velox/API/V3dLib/Utils/VStringValue.h
@@ -2,7 +2,9 @@
 #define V3D_VSTRINGVALUE_08_31_04_H
 //-----------------------------------------------------------------------------
 #include <v3d/Core/VCoreLib.h>
+#include <v3d/Core/VException.h>
 #include <string>
+#include <sstream>
 //-----------------------------------------------------------------------------
 namespace v3d {
 namespace utils {
@@ -14,19 +16,85 @@ namespace utils {
 	 * @author: ins
 	 */
 
+V3D_DECLARE_EXCEPTION(VStringValueException, VException);
+
 class VStringValue
 {
 public:
+	VStringValue()
+	{
+		m_sValue = "";
+	}
 
-	VStringValue(VStringParam in_Value);
+	VStringValue(VStringParam in_Value)
+	{
+		m_sValue = in_Value;
+	}
 	
-	vfloat32 AsFloat();
-	vint32   AsInt();
-	VStringRetVal AsString();
+	template<typename T>
+		void Set(const T val)
+	{
+		std::stringstream str;
+		str << val;
+		m_sValue = str.str();
+	}
 
+	template <typename T>
+		T Get() const
+	{
+		T returnValue;
+
+		if(ConvertString<T>(returnValue, m_sValue, std::dec))
+			return returnValue;
+		else
+			return T();
+	}
+
+	template <typename T>
+		T Get() 
+	{
+		T returnValue;
+
+		if(ConvertString<T>(returnValue, m_sValue, std::dec))
+			return returnValue;
+		else
+			return T();
+	}
+
+	template<typename T>
+		T GetSafe() const
+	{
+		T returnValue;
+
+		if(ConvertString<T>(returnValue, m_sValue, std::dec))
+			return returnValue;
+		else
+			V3D_THROW(VStringValueException, "invalid string to convert!");
+	}
+
+	template<typename T>
+		T GetSafe()
+	{
+		T returnValue;
+
+		if(ConvertString<T>(returnValue, m_sValue, std::dec))
+			return returnValue;
+		else
+			V3D_THROW(VStringValueException, "invalid string to convert!");
+	}
 private:
 	
 	std::string m_sValue;
+
+	template <class T>
+		vbool ConvertString(
+		T& t,
+		const std::string &s,
+		std::ios_base &(*f)(std::ios_base&))
+	{
+		std::istringstream iss(s);
+		return !(iss>>f>>t).fail();
+	}
 };
 
 //-----------------------------------------------------------------------------
