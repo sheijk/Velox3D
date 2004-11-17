8016d1a7014d066cddebb2e5b7bf571d4f82fe85
insane <insane@165746f0-bcf8-0310-b0ad-99815d7186ab>
2004-11-17 22:41:36 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@371 165746f0-bcf8-0310-b0ad-99815d7186ab

--- velox/API/V3dLib/Graphics/Importer/VQ3BspImporter.h
+++ velox/API/V3dLib/Graphics/Importer/VQ3BspImporter.h
@@ -8,27 +8,41 @@
 namespace v3d {
 namespace utils{
 //-----------------------------------------------------------------------------
-//wingdi spack rum....
+//wingdi spackt rum....
 #undef PLANES
 
-class VQ3BspImporter
+class VQ3BSPImporter
 {
 public:
-	VQ3BspImporter();
-	~VQ3BspImporter();
+	VQ3BSPImporter();
+	~VQ3BSPImporter();
 
 
-	vbool LoadQ3Level(VStringParam in_sFilename);
+	/**
+	 * Loads the given file from disk
+	 */
 
-// TODO: FIX GLOBAL ACCESSING
-//private:
-
-
-	/* Delete our alloced mem */
-	void Delete();
+	vbool Create(VStringParam in_sFilename);
 	
-	/* Swap axis for normal use */
-	void SwapAxis();  // TODO: nur um jan zur aergern :))
+	/**
+	 * Swap axis for normal use 
+	 */
+
+	void SwapAxis();
+
+	/**
+	 * Getter's for loaded types
+	 */
+
+	vint GetVertexCount();
+	vint GetFaceCount();
+	vint GetTextureCount();
+	vint GetLightmapCount();
+	vint GetNodeCount();
+	vint GetLeafCount();
+	vint GetLeafFaceCount();
+	vint GetPlaneCount();
+	
 
 	enum Settings
 	{
@@ -64,6 +78,11 @@ public:
 		graphics::VTexCoord2f LightmapCoord;
 		graphics::VVector3f Normal;
 		vbyte ColorData[4];
+	};
+
+	struct BSPVector
+	{
+		int x,y,z;
 	};
 
 	struct BSPHeader
@@ -112,19 +131,79 @@ public:
 		vbyte LightmapData[49152]; //128*128*3
 	};
 
+	struct BSPNode
+	{
+		int iPLane;
+		int iFront;
+		int iBack;
+        BSPVector min;
+		BSPVector max;
+	};
+
+	struct BSPLeaf
+	{
+		vint iCluster;
+		vint iArea;
+		BSPVector min;
+		BSPVector max;
+		vint iLeafFace;
+		vint iNumLeafFaces;
+		vint iLeafBrush;
+		vint iNumLeafBrushes;
+	};
+
+	struct BSPPlane
+	{
+		graphics::VVector3f Normal;
+		vfloat32 fDistance;
+	};
+
+	struct BSPPVS
+	{
+		vint	iNumClusters;
+		int		iClusterbytes;
+		vbyte*	pCluster;
+	};
+
+
+	BSPVertex*		GetVertexPointer();
+	BSPFace*		GetFacePointer();
+	BSPTexture*		GetTexturePointer();
+	BSPLightmap*	GetLightmapPointer();
+	BSPNode*		GetNodePointer();
+	BSPLeaf*		GetLeafPointer();
+	vint*			GetLeafFacePointer();
+	BSPPlane*		GetPlanePointer();
+	BSPPVS*			GetPVSData();
+
+private:
+
+	/* Delete our alloced mem */
+	void DeleteRawData();
+
+
 
 	vint m_iNumVertices;
-	vint m_iNumFaces;
+    vint m_iNumFaces;
 	vint m_iNumTextures;
 	vint m_iNumLightmaps;
-
+	vint m_iNumNodes;
+	vint m_iNumLeafs;
+	vint m_iNumLeafFaces;
+	vint m_iNumPlanes;
+    
 	
 	BSPFace*     m_pFaces;
 	BSPVertex*   m_pVertices;
 	BSPTexture*  m_pTextures;
 	BSPLightmap* m_pLightmaps;
+	BSPNode*	 m_pNodes;
+	BSPLeaf*	 m_pLeaf;
+	BSPPlane*	 m_pPlanes;
+	vint*		 m_pLeafFaces;
+	BSPPVS		 m_PVSData;
 
-	vbyte* m_pRawData;
+	vbyte*		 m_pRawData;
 
 };
 //-----------------------------------------------------------------------------
