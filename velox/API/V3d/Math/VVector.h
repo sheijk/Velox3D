6d4870d23f44e50b044b3f62e43962720da16efb
Lord_Adres <Lord_Adres@165746f0-bcf8-0310-b0ad-99815d7186ab>
2004-12-11 02:44:32 +0000

    Spezalisierung f�r VVector<vfloat32, 3>
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@379 165746f0-bcf8-0310-b0ad-99815d7186ab

--- velox/API/V3d/Math/VVector.h
+++ velox/API/V3d/Math/VVector.h
@@ -227,6 +227,18 @@ namespace v3d {
 	{
 		gmtl::normalize(vec.m_Vec);
 	}
+
+	template<typename Scalar, vuint Dimension>
+	VVector<Scalar, Dimension> Normalized(
+		const VVector<Scalar, Dimension>& vec
+		)
+	{
+		VVector<Scalar, Dimension> ret(vec);
+		Normalize(ret);
+		return ret;
+	}
+
+
 	
 	template<typename Scalar, vuint Dimension>
 	Scalar Length(
@@ -244,7 +256,242 @@ namespace v3d {
 		return gmtl::lengthSquared(vec.m_Vec);
 	}
 
-		
+//------------------------------------------------------------------------------
+	template<typename Scalar>
+	class VVector<Scalar, 3>
+	{
+	public:
+		// do not use members and functions in this block as they are only
+		// public because of some problem with friend declarations
+		typedef gmtl::Vec<Scalar, 3> VecImpl;
+
+		VecImpl m_Vec;
+
+		VVector(const VecImpl& implv) : m_Vec(implv) {}
+	public:
+		VVector() {}
+		~VVector() {}
+		VVector(const VVector& source) : m_Vec(source.m_Vec) {}
+
+		VVector(Scalar in_X, Scalar in_Y, Scalar in_Z)
+		{
+			Set(0, in_X);
+			Set(1, in_Y);
+			Set(2, in_Z);
+		}
+
+		void Set(vuint in_Pos, Scalar in_Val)
+		{
+			m_Vec[in_Pos] = in_Val;
+		}
+
+		Scalar Get(vuint in_Pos)
+		{
+			return m_Vec[in_Pos];
+		}
+
+		Scalar& operator[](vuint in_Pos)
+		{
+			return m_Vec[in_Pos];
+		}
+
+		const Scalar& operator[](vuint in_Pos) const
+		{
+			return m_Vec[in_Pos];
+		}
+
+		vuint GetDimension()
+		{
+			return 3;
+		}
+
+		VVector& operator=(const VVector& source)
+		{
+			m_Vec = source.m_Vec;
+			return *this;
+		}
+
+		//friend void Add(VVector&, const VVector&, const VVector&);
+		//friend void Sub(VVector&, const VVector&, const VVector&);
+		//friend void ApplyCross(VVector&, const VVector&, const VVector&);
+		//friend VVector Cross(const VVector&, const VVector&);
+		//friend Scalar Dot(const VVector&, const VVector&);
+		//
+		//template<typename Scalar, vuint Dimension, typename AssignType>
+		//friend void Mult(VVector&, AssignType);
+
+		//template<typename Scalar, vuint Dimension, typename AssignType>
+		//friend VVector operator*(const VVector&, AssignType);
+
+		//template<typename Scalar, vuint Dimension, typename AssignType>
+		//friend VVector operator*(AssignType, const VVector&);
+
+		//friend void Normalize(VVector&);
+		//friend Scalar Length(const VVector&);
+		//friend Scalar LengthSquared(const VVector&);
+
+	};
+
+	template<typename Scalar>
+		void Add(
+		VVector<Scalar, 3>& res,
+		const VVector<Scalar, 3>& a,
+		const VVector<Scalar, 3>& b
+		)
+	{
+		res.m_Vec = a.m_Vec + b.m_Vec;
+		//gmtl::add(res.m_Vec, a.m_Vec, b.m_Vec);
+	}
+
+	template<typename Scalar>
+		void Sub(
+		VVector<Scalar, 3>& res,
+		const VVector<Scalar, 3>& a,
+		const VVector<Scalar, 3>& b
+		)
+	{
+		res.m_Vec = a.m_Vec - b.m_Vec;
+		//gmtl::sub(res.m_Vec, a.m_Vec, b.m_Vec);
+	}
+
+	template<typename Scalar>
+		VVector<Scalar, 3>
+		operator+(
+		const VVector<Scalar, 3>& a,
+		const VVector<Scalar, 3>& b
+		)
+	{
+		VVector<Scalar> res;
+		Add(res, a, b);
+		return res;
+	}
+
+	template<typename Scalar>
+		VVector<Scalar, 3>
+		operator-(
+		const VVector<Scalar, 3>& a,
+		const VVector<Scalar, 3>& b
+		)
+	{
+		VVector<Scalar, 3> res;
+		Sub(res, a, b);
+		return res;
+	}
+
+	template<typename Scalar>
+		void operator+=(
+		VVector<Scalar, 3>& result,
+		const VVector<Scalar, 3>& addval
+		)
+	{
+		Add(result, result, addval);
+	}
+
+	template<typename Scalar>
+		void operator-=(
+		VVector<Scalar, 3>& result,
+		const VVector<Scalar, 3>& subval
+		)
+	{
+		Sub(result, result, subval);
+	}
+
+	template<typename Scalar>
+		void ApplyCross(
+		VVector<Scalar, 3>& result,
+		const VVector<Scalar, 3>& a,
+		const VVector<Scalar, 3>& b
+		)
+	{
+		gmtl::cross(result.m_Vec, a.m_Vec, b.m_Vec);
+	}
+
+	template<typename Scalar>
+		VVector<Scalar, 3>
+		Cross(
+		const VVector<Scalar, 3>& a,
+		const VVector<Scalar, 3>& b
+		)
+	{
+		return VVector<Scalar, 3>(gmtl::cross(a.m_Vec, b.m_Vec));
+	}
+
+	template<typename Scalar>
+		Scalar Dot(
+		const VVector<Scalar, 3>& a,
+		const VVector<Scalar, 3>& b
+		) 
+	{
+		Scalar res;
+		res = dot(a.m_Vec, b.m_Vec);
+		return res;
+		//return VVector<Scalar, Dimension>(gmtl::dot(a.m_Vec, b.m_Vec));
+	}
+
+	template<typename Scalar, typename AssignType>
+		void Mult(
+		VVector<Scalar, 3>& vec,
+		AssignType fac
+		)
+	{
+		gmtl::operator*=(vec.m_Vec, fac);
+	}
+
+	template<typename Scalar, typename AssignType>
+		void operator*=(
+		VVector<Scalar, 3>& vec,
+		AssignType fac
+		)
+	{
+		Mult(vec, fac);
+	}
+
+	template<typename Scalar, typename AssignType>
+		VVector<Scalar, 3> 
+		operator*(
+		const VVector<Scalar, 3>& vec,
+		AssignType fac
+		)
+	{
+		VVector<Scalar, 3> res(vec);
+		Mult(res, fac);
+		return res;		
+	}
+
+	template<typename Scalar, typename AssignType>
+		VVector<Scalar, 3>
+		operator*(
+		AssignType fac,
+		const VVector<Scalar, 3>& vec
+		)
+	{
+		return vec * fac;
+	}
+/*
+	template<typename Scalar>
+		void Normalize(
+		VVector<Scalar, 3>& vec
+		)
+	{
+		gmtl::normalize(vec.m_Vec);
+	}
+
+	template<typename Scalar>
+		Scalar Length(
+		const VVector<Scalar, 3>& vec
+		)
+	{
+		return gmtl::length(vec.m_Vec);
+	}
+
+	template<typename Scalar>
+		inline Scalar LengthSquared(
+		const VVector<Scalar, 3>& vec
+		)
+	{
+		return gmtl::lengthSquared(vec.m_Vec);
+	}
+*/		
 
 //-----------------------------------------------------------------------------
 } // namespace v3d
