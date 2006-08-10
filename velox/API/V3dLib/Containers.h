181c6aebe4a46747427be1af1efc17d4e63c18d9
sheijk <sheijk@165746f0-bcf8-0310-b0ad-99815d7186ab>
2006-08-10 21:00:02 +0000

    die recursively!
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@614 165746f0-bcf8-0310-b0ad-99815d7186ab

--- velox/API/V3dLib/Containers.h
+++
@@ -1,9 +0,0 @@
-#ifndef V3D_CONTAINERS_2004_04_19_H
-#define V3D_CONTAINERS_2004_04_19_H
-//-----------------------------------------------------------------------------
-#include <v3d/Core/VCoreLib.h>
-
-#include <V3dLib/Containers/VArray2d.h>
-
-//-----------------------------------------------------------------------------
-#endif // V3D_CONTAINERS_2004_04_19_H
--- velox/API/V3dLib/Property.h
+++
@@ -1,9 +0,0 @@
-#ifndef V3D_PROPERTY_2004_09_25_H
-#define V3D_PROPERTY_2004_09_25_H
-//-----------------------------------------------------------------------------
-
-#include <V3dLib/Property/VProperty.h>
-#include <V3dLib/Property/VPropertyManager.h>
-
-//-----------------------------------------------------------------------------
-#endif // V3D_PROPERTY_2004_09_25_H
