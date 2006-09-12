ce98a2b35c431b99ee4df820c2543652c8fe10f9
sheijk <sheijk@165746f0-bcf8-0310-b0ad-99815d7186ab>
2006-09-12 01:43:32 +0000

    fixed a script
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@627 165746f0-bcf8-0310-b0ad-99815d7186ab

--- velox/Workspace/Scripts/v3dstats.sh
+++ velox/Workspace/Scripts/v3dstats.sh
@@ -7,9 +7,9 @@
 
 cd `v3dgetdir.sh`
 
-veloxLines=`cat \`/bin/find API/ Source/ -regex ".*\.\(h\|cpp\|inl\)"\` | wc -l`
-editorLines=`cat \`/bin/find Editor/EclipsePlugin/ -regex ".*\.java"\` | wc -l`
-exampleLines=`cat \`/bin/find Examples/ -regex ".*\.\(h\|cpp\|inl\)"\` | wc -l`
+veloxLines=`cat \`find API/ Source/ -regex ".*\.\(h\|cpp\|inl\)"\` | wc -l`
+editorLines=`cat \`find Editor/EclipsePlugin/ -regex ".*\.java"\` | wc -l`
+exampleLines=`cat \`find Examples/ -regex ".*\.\(h\|cpp\|inl\)"\` | wc -l`
 
 echo Total lines in velox: $veloxLines
 echo Total lines in editor: $editorLines
