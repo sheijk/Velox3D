545bc98ebcbe72a3f7637923d2407015560049c8
sheijk <sheijk@165746f0-bcf8-0310-b0ad-99815d7186ab>
2007-05-18 19:59:30 +0000

    - missing files
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@686 165746f0-bcf8-0310-b0ad-99815d7186ab

---
+++ velox/Source/Graphics/OpenGL/Context/VSubAreaContext.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2002-2006 Velox Development Team. This file is licenced under the
+ * revised BSD licence. See licence_bsd.txt in the root of the Velox 
+ * distribution or http://www.sechsta-sinn.de/velox/licence_bsd.txt for the
+ * complete licence text
+ */
+#include "VSubAreaContext.h"
+//-----------------------------------------------------------------------------
+#include <V3d/OpenGL.h>
+#include <V3d/Core/VException.h>
+//-----------------------------------------------------------------------------
+#include <V3d/Core/MemManager.h>
+//-----------------------------------------------------------------------------
+namespace v3d { namespace graphics {
+//-----------------------------------------------------------------------------
+using namespace v3d; // anti auto indent
+
+/**
+ * standard c'tor
+ */
+VSubAreaContext::VSubAreaContext(VSharedPtr<IVRenderContext> in_pParentContext)
+{
+	m_pParentContext = in_pParentContext;
+}
+
+/**
+ * d'tor
+ */
+VSubAreaContext::~VSubAreaContext()
+{
+}
+
+void VSubAreaContext::SetArea(vint left, vint top, vint right, vint bottom)
+{
+	m_nLeft = left;
+	m_nTop = top;
+	m_nRight = right;
+	m_nBottom = bottom;
+}
+
+void VSubAreaContext::SetReallySwap(vbool yesno)
+{
+	m_bReallySwap = yesno;
+}
+
+vbool VSubAreaContext::GetReallySwap()
+{
+	return m_bReallySwap;
+}
+
+void VSubAreaContext::MakeCurrent()
+{
+	if( m_pParentContext != 0 )
+	{
+		m_pParentContext->MakeCurrent();
+
+		//glViewport( m_nLeft, m_nTop, m_nRight - m_nLeft, m_nTop - m_nBottom );
+	}
+}
+
+void VSubAreaContext::SwapBuffers()
+{
+	if( m_bReallySwap && m_pParentContext != 0 )
+	{
+		m_pParentContext->SwapBuffers();
+	}
+}
+
+VDisplaySettings* VSubAreaContext::GetDisplaySettings()
+{
+	if( m_pParentContext != 0 )
+	{
+		return m_pParentContext->GetDisplaySettings();
+	}
+	else
+	{
+		V3D_THROW(VException, "Could not return display settings, parent context is null");
+	}
+}
+
+IVRenderContext* VSubAreaContext::CreateOffscreenContext(const VDisplaySettings* in_pDisplaySettings)
+{
+	if( m_pParentContext != 0 )
+	{
+		return m_pParentContext->CreateOffscreenContext(in_pDisplaySettings);
+	}
+	else
+	{
+		V3D_THROW(VException, "Could not create offscreen context, parent context is null");
+	}
+}
+
+//-----------------------------------------------------------------------------
+}} // namespace v3d::graphics
+//-----------------------------------------------------------------------------
+
---
+++ velox/Source/Graphics/OpenGL/Context/VSubAreaContext.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2002-2006 Velox Development Team. This file is licenced under the
+ * revised BSD licence. See licence_bsd.txt in the root of the Velox 
+ * distribution or http://www.sechsta-sinn.de/velox/licence_bsd.txt for the
+ * complete licence text
+ */
+#ifndef V3D_VSUBAREACONTEXT_2007_05_17_H
+#define V3D_VSUBAREACONTEXT_2007_05_17_H
+//-----------------------------------------------------------------------------
+#include <V3d/Core/VCoreLib.h>
+
+#include <V3d/Graphics/IVRenderContext.h>
+#include <V3d/Core/SmartPtr/VSharedPtr.h>
+//-----------------------------------------------------------------------------
+namespace v3d { namespace graphics {
+//-----------------------------------------------------------------------------
+using namespace v3d; // anti auto indenting
+
+/**
+ * Renders to a subarea of the parent's context using glViewport
+ *
+ * Attention: untested and never used. Be careful! :)
+ *
+ * @author sheijk
+ */
+class VSubAreaContext : public IVRenderContext
+{
+public:
+	VSubAreaContext(VSharedPtr<IVRenderContext> in_pParentContext);
+	virtual ~VSubAreaContext();
+
+	virtual void MakeCurrent();
+	virtual void SwapBuffers();
+	virtual VDisplaySettings* GetDisplaySettings();
+	virtual IVRenderContext* CreateOffscreenContext(const VDisplaySettings* in_pDisplaySettings);
+
+	void SetArea(vint left, vint top, vint right, vint bottom);
+	void SetReallySwap(vbool yesno);
+	vbool GetReallySwap();
+private:
+	VSharedPtr<IVRenderContext> m_pParentContext;
+
+	/// propagate swap call to parent context yes/no
+	vbool m_bReallySwap;
+
+	vint m_nLeft;
+	vint m_nTop;
+	vint m_nRight;
+	vint m_nBottom;
+};
+
+//-----------------------------------------------------------------------------
+}} // namespace v3d::graphics
+//-----------------------------------------------------------------------------
+#endif // V3D_VSUBAREACONTEXT_2007_05_17_H
+
