21e8a54e2d0a9825aac46ea91794e1b70125d2e0
sheijk <sheijk@165746f0-bcf8-0310-b0ad-99815d7186ab>
2007-08-08 23:41:37 +0000

    - batch files to test scenes in debug and release build standalone program
    git-svn-id: svn+ssh://131.220.23.26/daten/v3d/trunk@734 165746f0-bcf8-0310-b0ad-99815d7186ab

---
+++ velox/Data/RunScened.bat
@@ -0,0 +1,13 @@
+@echo Please enter the name and path of the scene you want to start
+@echo Use the velox vfs name
+@echo Example: /edit/simple to load ./workspace/edit/simple.v3dscene
+
+::@FOR /F "tokens=*" %%A IN ('TYPE CON') DO SET INPUT=%%A
+@set /p INPUT=
+:: Use quotes if you want to display redirection characters as well
+@echo Starting scene "%INPUT%"
+
+@cd Workspace
+@..\..\Local\Build8\debug\RacerDemo.exe -scene %INPUT%.v3dscene
+@cd ..
+@pause
